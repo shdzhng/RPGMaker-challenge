23c47e25d195a968003b968f6d62a7525c1d510b
Shou Zhang <shoud.zhang@gmail.com>
2022-10-04 14:00:38 -0700

    implement navbar with custom dark theme slider

--- styles/globalStyles.js
+++ styles/globalStyles.js
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
+import breakpoints from './breakpoints';
 
 export const lightTheme = {
   text: '#3D405B',
@@ -6,13 +7,14 @@ export const lightTheme = {
   background: {
     primary: '#F4F1DE',
     secondary: '#81B29A',
+    contrast: '#264653',
   },
 };
 
 export const darkTheme = {
   text: '#F4F1DE',
   accent: '#E9C46A',
-  background: { primary: '#264653', secondary: '#3D405B' },
+  background: { primary: '#264653', secondary: '#2A9D8F', contrast: '#F4F1DE' },
 };
 
 export const GlobalStyles = createGlobalStyle`
@@ -25,12 +27,19 @@ html,
 body {
   padding: 0;
   margin: 0;
+
 }
 
   body {
     background: ${({ theme }) => theme.body};
     color: ${({ theme }) => theme.text};
     font-family: Tahoma, Helvetica, Arial, Roboto, sans-serif;
+    @media (${breakpoints.device.sm}) {
+      font-size: 0.9rem;
+    }
+    @media (${breakpoints.device.xs}) {
+      font-size: 0.8rem;
+    }
   }
 
   span{background-color: ${({ theme }) => theme.accent}}
