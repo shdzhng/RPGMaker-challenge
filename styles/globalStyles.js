6c77ed0826b8e6ff93c8c5da1ab8c50ffc4c2c78
Shou Zhang <shoud.zhang@gmail.com>
2022-10-04 18:12:19 -0700

    add animation and responsive styling

--- styles/globalStyles.js
+++ styles/globalStyles.js
@@ -2,19 +2,30 @@ import { createGlobalStyle } from 'styled-components';
 import breakpoints from './breakpoints';
 
 export const lightTheme = {
-  text: '#3D405B',
+  text: {
+    primary: '#3D405B',
+    secondary: '#F4F1DE',
+  },
   accent: '#E07A5F',
   background: {
     primary: '#F4F1DE',
     secondary: '#81B29A',
     contrast: '#264653',
+    accent: '#e9c46a',
   },
+  disabled: '#9a9a9a',
 };
 
 export const darkTheme = {
-  text: '#F4F1DE',
+  text: { primary: '#F4F1DE', secondary: '#fefae0' },
   accent: '#E9C46A',
-  background: { primary: '#264653', secondary: '#2A9D8F', contrast: '#F4F1DE' },
+  background: {
+    primary: '#264653',
+    secondary: '#2A9D8F',
+    contrast: '#F4F1DE',
+    accent: '#E07A5F',
+  },
+  disabled: '#9a9a9a',
 };
 
 export const GlobalStyles = createGlobalStyle`
@@ -32,7 +43,7 @@ body {
 
   body {
     background: ${({ theme }) => theme.body};
-    color: ${({ theme }) => theme.text};
+    color: ${({ theme }) => theme.text.primary};
     font-family: Tahoma, Helvetica, Arial, Roboto, sans-serif;
     @media (${breakpoints.device.sm}) {
       font-size: 0.9rem;
@@ -42,14 +53,22 @@ body {
     }
   }
 
-  span{background-color: ${({ theme }) => theme.accent}}
+  span{
+    color: ${({ theme }) => theme.accent};
+
+  }
+
+  fieldset{
+    border:none;
+  }
 
 a {
   color: inherit;
   text-decoration: none;
 }
 
+
   h1{
-     color: ${({ theme }) => theme.text};
+     color: ${({ theme }) => theme.text.primary};
   }
 `;
